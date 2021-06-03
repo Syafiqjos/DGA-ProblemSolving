60297be40b0b454aba959ae7fa47206d5e73cde0
Syafiqjos <syafiqjos@gmail.com>
2021-06-03 13:21:04 +0700

    Ball follow mouse position

--- ProblemSolving/Assets/Scripts/BallMovement.cs
+++ ProblemSolving/Assets/Scripts/BallMovement.cs
@@ -9,14 +9,22 @@ public class BallMovement : MonoBehaviour
     public Vector2 direction;
     public float speed = 50.0f;
 
+    private Camera mainCamera;
+
     private void Awake()
     {
         rb2 = GetComponent<Rigidbody2D>();
+        mainCamera = Camera.main;
     }
 
     private void FixedUpdate()
     {
-        direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * speed;
-        rb2.velocity = direction;
+        //direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * speed;
+
+        direction = (mainCamera.ScreenToWorldPoint(Input.mousePosition) - transform.position).normalized;
+        //Vector3 target = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+
+        //rb2.velocity = direction;
+        rb2.MovePosition((Vector2)transform.position + direction * speed);
     }
 }
